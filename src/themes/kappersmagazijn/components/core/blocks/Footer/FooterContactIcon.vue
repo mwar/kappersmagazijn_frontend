3af378379c93979e74eace822ff274ec4bfa67da
Martijn Warrink <info@mwar.nl>
2020-11-06 17:53:22 +0100

    Fooooter.. (en default maar mee gecommit)

---
+++ src/themes/kappersmagazijn/components/core/blocks/Footer/FooterContactIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h6><i :style="'background-image: url(' + icon + ')'" class="call-us"/>{{ title }}</h6>
+    <a :href="url"><span>{{ description }}</span></a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  text-align: left;
+}
+
+h6 {
+  i.call-us {
+    display: inline-block;
+    //background-image: url(/assets/icons/search-regular.svg);
+    background-size: 20px 20px;
+    height: 20px;
+    width: 20px;
+    background-repeat: no-repeat;
+    //margin: 0 auto 5px auto;
+    margin-right: 20px;
+  }
+
+  line-height: 15px;
+  margin: auto auto 15px;
+  font-weight: normal;
+  font-size: large;
+}
+
+a {
+  color: #504538;
+  text-align: left;
+}
+</style>
+
+<script>
+export default {
+  props: [
+    'title',
+    'description',
+    'url',
+    'icon'
+  ]
+
+}
+</script>
--- src/themes/kappersmagazijn/components/core/blocks/Footer/Newsletter.vue
+++ src/themes/kappersmagazijn/components/core/blocks/Footer/Newsletter.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="newsletter py25 px15 bg-cl-secondary" v-show="!isSubscribed">
-    <div class="container">
-      <div class="newsletter-content m0 row middle-sm start-md">
-        <div class="col-md-8 col-xs-12">
-          <h3 class="h3 cl-accent weight-400 m0">
-            {{ $t('Subscribe to the newsletter and receive a coupon for 10% off') }}
-          </h3>
-        </div>
-        <div class="newsletter-button col-md-4 col-xs-12 end-md">
-          <button-outline
-            @click.native="showNewsletterPopup"
-            color="dark"
-            data-testid="openNewsletterButton"
-          >
-            {{ $t('Subscribe') }}
-          </button-outline>
-        </div>
-      </div>
-    </div>
-    <newsletter-popup v-if="loadNewsletterPopup" />
+    <form @submit.prevent="subscribe(onSuccesfulSubmission)" novalidate>
+      <p class="h4">
+        {{ $t('Sign up to our newsletter') }}
+      </p>
+      <base-input
+        focus
+        type="email"
+        name="email"
+        v-model="email"
+        autocomplete="email"
+        :placeholder="$t('E-mail address *')"
+        :validations="[
+              {
+                condition: $v.email.$error && !$v.email.required,
+                text: $t('Field is required.')
+              },
+              {
+                condition: !$v.email.email && $v.email.$error,
+                text: $t('Please provide valid e-mail address.')
+              }
+            ]"
+      />
+      <button-full
+        class="mb35"
+        type="submit"
+        :disabled="this.$v.$invalid"
+        @click.native="$v.email.$touch"
+      >
+        {{ $t('Subscribe') }}
+      </button-full>
+    </form>
   </div>
 </template>
 
@@ -26,12 +38,15 @@
 import SubscriptionStatus from '@vue-storefront/core/modules/newsletter/mixins/SubscriptionStatus'
 import ButtonOutline from 'theme/components/theme/ButtonOutline'
 import { mapState } from 'vuex'
+import ButtonFull from 'theme/components/theme/ButtonFull.vue'
+import BaseInput from 'theme/components/core/blocks/Form/BaseInput.vue'
+
 const NewsletterPopup = () => import(/* webpackChunkName: "vsf-newsletter-modal" */ 'theme/components/core/NewsletterPopup.vue')
 
 export default {
   name: 'Newsletter',
   mixins: [SubscriptionStatus],
-  data () {
+  data() {
     return {
       loadNewsletterPopup: false
     }
@@ -42,32 +57,36 @@ export default {
     })
   },
   methods: {
-    showNewsletterPopup () {
+    showNewsletterPopup() {
       this.loadNewsletterPopup = true
       this.$bus.$emit('modal-show', 'modal-newsletter')
     }
   },
   components: {
     ButtonOutline,
-    NewsletterPopup
+    NewsletterPopup,
+    ButtonFull,
+    BaseInput
   }
 }
 </script>
 
 <style scoped>
-  @media (min-width: 767px) and (max-width: 1200px){
-    .button-outline{
-      min-width: 100%;
-    }
+@media (min-width: 767px) and (max-width: 1200px) {
+  .button-outline {
+    min-width: 100%;
   }
-  @media (max-width: 767px) {
-    .h3 {
-      font-size: 18px;
-      text-align: center;
-    }
-    .newsletter-button {
-      padding-top: 25px;
-      text-align: center;
-    }
+}
+
+@media (max-width: 767px) {
+  .h3 {
+    font-size: 18px;
+    text-align: center;
   }
+
+  .newsletter-button {
+    padding-top: 25px;
+    text-align: center;
+  }
+}
 </style>
